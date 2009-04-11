82dbedc7212ccd7ee12a991770839364623a7687
SoulKeeper <SoulKeeper@2af79824-a935-44bf-bf41-fd8ed1897d0c>
2009-04-11 15:18:41 +0000

    code formatting
    git-svn-id: http://my-svn.assembla.com/svn/aion-emu/trunk@89 2af79824-a935-44bf-bf41-fd8ed1897d0c

--- AE_LoginServer/data/scripts/database/mysql5/MySQL5AccountDAO.java
+++ AE_LoginServer/data/scripts/database/mysql5/MySQL5AccountDAO.java
@@ -17,224 +17,272 @@ import com.aionemu.loginserver.model.Account;
 
 /**
  * MySQL5 Account DAO implementation
+ * 
  * @author SoulKeeper
  */
-public class MySQL5AccountDAO extends AccountDAO {
+public class MySQL5AccountDAO extends AccountDAO
+{
 
-    /**
-     * Logger
-     */
-    private static final Logger log = Logger.getLogger(MySQL5AccountDAO.class);
+	/**
+	 * Logger
+	 */
+	private static final Logger	log	= Logger.getLogger(MySQL5AccountDAO.class);
 
-    /**
-     * Register dao on class load
-     */
-    @OnClassLoad
-    public static void onLoad(){
-        try {
-            DAOManager.registerDAO(MySQL5AccountDAO.class);
-        } catch (IllegalAccessException e) {
-            log.error("Can't register DAO", e);
-        } catch (InstantiationException e) {
-            log.error("Can't register DAO", e);
-        }
-    }
+	/**
+	 * Register dao on class load
+	 */
+	@OnClassLoad
+	public static void onLoad()
+	{
+		try
+		{
+			DAOManager.registerDAO(MySQL5AccountDAO.class);
+		}
+		catch (IllegalAccessException e)
+		{
+			log.error("Can't register DAO", e);
+		}
+		catch (InstantiationException e)
+		{
+			log.error("Can't register DAO", e);
+		}
+	}
 
-    /**
-     * Unregister DAO class
-     */
-    @OnClassUnload
-    public static void onUnload(){
-        DAOManager.unregisterDAO(MySQL5AccountDAO.class);
-    }
+	/**
+	 * Unregister DAO class
+	 */
+	@OnClassUnload
+	public static void onUnload()
+	{
+		DAOManager.unregisterDAO(MySQL5AccountDAO.class);
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public Account getAccount(String name) {
-        Account account = null;
-        PreparedStatement st = DB.prepareStatement("SELECT * FROM account_data WHERE `name` = ?");
-        try{
-            st.setString(1, name);
-            ResultSet rs = st.executeQuery();
-            if(rs.next()){
-                account = new Account();
-                account.setId(rs.getInt("id"));
-                account.setName(name);
-                account.setPasswordHash(rs.getString("password"));
-                account.setLastActive(rs.getTimestamp("last_active"));
-                account.setExpirationTime(rs.getTimestamp("expiration_time"));
-                account.setPenaltyEnd(rs.getTimestamp("penalty_end"));
-                account.setAccessLevel(rs.getByte("access_level"));
-                account.setLastServer(rs.getByte("last_server"));
-                account.setLastIp(rs.getString("last_ip"));
-                account.setIpForce(rs.getString("ip_force"));
-            }
-        } catch(Exception e){
-            log.error("Can't select account with name: " + name, e);
-        } finally {
-            DB.close(st);
-        }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Account getAccount(String name)
+	{
+		Account account = null;
+		PreparedStatement st = DB.prepareStatement("SELECT * FROM account_data WHERE `name` = ?");
+		try
+		{
+			st.setString(1, name);
+			ResultSet rs = st.executeQuery();
+			if (rs.next())
+			{
+				account = new Account();
+				account.setId(rs.getInt("id"));
+				account.setName(name);
+				account.setPasswordHash(rs.getString("password"));
+				account.setLastActive(rs.getTimestamp("last_active"));
+				account.setExpirationTime(rs.getTimestamp("expiration_time"));
+				account.setPenaltyEnd(rs.getTimestamp("penalty_end"));
+				account.setAccessLevel(rs.getByte("access_level"));
+				account.setLastServer(rs.getByte("last_server"));
+				account.setLastIp(rs.getString("last_ip"));
+				account.setIpForce(rs.getString("ip_force"));
+			}
+		}
+		catch (Exception e)
+		{
+			log.error("Can't select account with name: " + name, e);
+		}
+		finally
+		{
+			DB.close(st);
+		}
 
-        return account;
-    }
+		return account;
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public int getAccountId(String name) {
-        int id = -1;
-        PreparedStatement st = DB.prepareStatement("SELECT `id` FROM account_data WHERE `name` = ?");
-        try {
-            st.setString(1, name);
-            ResultSet rs = st.executeQuery();
-            rs.next();
-            id = rs.getInt("id");
-        } catch (SQLException e) {
-            log.error("Can't select id after account insertion", e);
-        }
-        finally {
-            DB.close(st);
-        }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public int getAccountId(String name)
+	{
+		int id = -1;
+		PreparedStatement st = DB.prepareStatement("SELECT `id` FROM account_data WHERE `name` = ?");
+		try
+		{
+			st.setString(1, name);
+			ResultSet rs = st.executeQuery();
+			rs.next();
+			id = rs.getInt("id");
+		}
+		catch (SQLException e)
+		{
+			log.error("Can't select id after account insertion", e);
+		}
+		finally
+		{
+			DB.close(st);
+		}
 
-        return id;
-    }
+		return id;
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public int getAccountCount() {
-        PreparedStatement st = DB.prepareStatement("SELECT count(*) AS c FROM account_data");
-        ResultSet rs = DB.executeQuerry(st);
-        try {
-            rs.next();
-            return rs.getInt("c");
-        } catch (SQLException e) {
-            log.error("Can't get account count", e);
-        } finally {
-            DB.close(st);
-        }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public int getAccountCount()
+	{
+		PreparedStatement st = DB.prepareStatement("SELECT count(*) AS c FROM account_data");
+		ResultSet rs = DB.executeQuerry(st);
+		try
+		{
+			rs.next();
+			return rs.getInt("c");
+		}
+		catch (SQLException e)
+		{
+			log.error("Can't get account count", e);
+		}
+		finally
+		{
+			DB.close(st);
+		}
 
-        return -1;
-    }
+		return -1;
+	}
 
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean insertAccount(Account account)
+	{
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public boolean insertAccount(Account account) {
+		int result = 0;
+		PreparedStatement st = DB
+			.prepareStatement("INSERT INTO account_data(`name`, `password`, last_active, expiration_time, penalty_end, access_level, last_server, last_ip, ip_force) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+		try
+		{
+			st.setString(1, account.getName());
+			st.setString(2, account.getPasswordHash());
+			st.setTimestamp(3, account.getLastActive());
+			st.setTimestamp(4, account.getExpirationTime());
+			st.setTimestamp(5, account.getPenaltyEnd());
+			st.setByte(6, account.getAccessLevel());
+			st.setByte(7, account.getLastServer());
+			st.setString(8, account.getLastIp());
+			st.setString(9, account.getIpForce());
+			result = st.executeUpdate();
+		}
+		catch (SQLException e)
+		{
+			log.error("Can't inser account", e);
+		}
+		finally
+		{
+			DB.close(st);
+		}
 
-        int result = 0;
-        PreparedStatement st = DB.prepareStatement("INSERT INTO account_data(`name`, `password`, last_active, expiration_time, penalty_end, access_level, last_server, last_ip, ip_force) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
-        try {
-            st.setString(1, account.getName());
-            st.setString(2, account.getPasswordHash());
-            st.setTimestamp(3, account.getLastActive());
-            st.setTimestamp(4, account.getExpirationTime());
-            st.setTimestamp(5, account.getPenaltyEnd());
-            st.setByte(6, account.getAccessLevel());
-            st.setByte(7, account.getLastServer());
-            st.setString(8, account.getLastIp());
-            st.setString(9, account.getIpForce());
-            result = st.executeUpdate();
-        } catch (SQLException e) {
-            log.error("Can't inser account", e);
-        } finally {
-            DB.close(st);
-        }
+		if (result > 0)
+		{
+			account.setId(getAccountId(account.getName()));
+		}
 
-        if (result > 0) {
-            account.setId(getAccountId(account.getName()));
-        }
+		return result > 0;
+	}
 
-        return result > 0;
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean updateAccount(Account account)
+	{
+		int result = 0;
+		PreparedStatement st = DB
+			.prepareStatement("UPDATE account_data SET `name` = ?, `password` = ?, last_active = ?, expiration_time = ?, penalty_end = ?, access_level = ?, last_server = ?, last_ip = ?, ip_force = ? WHERE `id` = ?");
+		try
+		{
+			st.setString(1, account.getName());
+			st.setString(2, account.getPasswordHash());
+			st.setTimestamp(3, account.getLastActive());
+			st.setTimestamp(4, account.getExpirationTime());
+			st.setTimestamp(5, account.getPenaltyEnd());
+			st.setByte(6, account.getAccessLevel());
+			st.setByte(7, account.getLastServer());
+			st.setString(8, account.getLastIp());
+			st.setString(9, account.getIpForce());
+			st.setInt(10, account.getId());
+			st.executeUpdate();
+		}
+		catch (SQLException e)
+		{
+			log.error("Can't update account");
+		}
+		finally
+		{
+			DB.close(st);
+		}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public boolean updateAccount(Account account) {
-        int result = 0;
-        PreparedStatement st = DB.prepareStatement("UPDATE account_data SET `name` = ?, `password` = ?, last_active = ?, expiration_time = ?, penalty_end = ?, access_level = ?, last_server = ?, last_ip = ?, ip_force = ? WHERE `id` = ?");
-        try {
-            st.setString(1, account.getName());
-            st.setString(2, account.getPasswordHash());
-            st.setTimestamp(3, account.getLastActive());
-            st.setTimestamp(4, account.getExpirationTime());
-            st.setTimestamp(5, account.getPenaltyEnd());
-            st.setByte(6, account.getAccessLevel());
-            st.setByte(7, account.getLastServer());
-            st.setString(8, account.getLastIp());
-            st.setString(9, account.getIpForce());
-            st.setInt(10, account.getId());
-            st.executeUpdate();
-        } catch (SQLException e) {
-            log.error("Can't update account");
-        } finally {
-            DB.close(st);
-        }
+		return result > 0;
+	}
 
-        return result > 0;
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean updateLastActive(final int accountId, final Timestamp time)
+	{
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public boolean updateLastActive(final int accountId, final Timestamp time) {
+		return DB.insertUpdate("UPDATE account_data SET last_active = ? WHERE id = ?", new IUStH() {
 
-        return DB.insertUpdate("UPDATE account_data SET last_active = ? WHERE id = ?", new IUStH() {
+			@Override
+			public void handleInsertUpdate(PreparedStatement preparedStatement) throws SQLException
+			{
+				preparedStatement.setTimestamp(1, time);
+				preparedStatement.setInt(2, accountId);
+				preparedStatement.execute();
+			}
+		});
+	}
 
-            @Override
-            public void handleInsertUpdate(PreparedStatement preparedStatement) throws SQLException {
-                preparedStatement.setTimestamp(1, time);
-                preparedStatement.setInt(2, accountId);
-                preparedStatement.execute();
-            }
-        });
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean updateLastServer(final int accountId, final byte lastServer)
+	{
+		return DB.insertUpdate("UPDATE account_data SET last_server = ? WHERE id = ?", new IUStH() {
+			@Override
+			public void handleInsertUpdate(PreparedStatement preparedStatement) throws SQLException
+			{
+				preparedStatement.setByte(1, lastServer);
+				preparedStatement.setInt(2, accountId);
+				preparedStatement.execute();
+			}
+		});
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public boolean updateLastServer(final int accountId, final byte lastServer) {
-        return DB.insertUpdate("UPDATE account_data SET last_server = ? WHERE id = ?", new IUStH() {
-            @Override
-            public void handleInsertUpdate(PreparedStatement preparedStatement) throws SQLException {
-                preparedStatement.setByte(1, lastServer);
-                preparedStatement.setInt(2, accountId);
-                preparedStatement.execute();
-            }
-        });
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean updateLastIp(final int accountId, final String ip)
+	{
+		return DB.insertUpdate("UPDATE account_data SET last_ip = ? WHERE id = ?", new IUStH() {
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public boolean updateLastIp(final int accountId, final String ip) {
-        return DB.insertUpdate("UPDATE account_data SET last_ip = ? WHERE id = ?", new IUStH() {
+			@Override
+			public void handleInsertUpdate(PreparedStatement preparedStatement) throws SQLException
+			{
+				preparedStatement.setString(1, ip);
+				preparedStatement.setInt(2, accountId);
+				preparedStatement.execute();
+			}
+		});
+	}
 
-            @Override
-            public void handleInsertUpdate(PreparedStatement preparedStatement) throws SQLException {
-                preparedStatement.setString(1, ip);
-                preparedStatement.setInt(2, accountId);
-                preparedStatement.execute();
-            }
-        });
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public boolean supports(String database, int majorVersion, int minorVersion) {
-        return MySQL5DAOUtils.supports(database, majorVersion, minorVersion);
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean supports(String database, int majorVersion, int minorVersion)
+	{
+		return MySQL5DAOUtils.supports(database, majorVersion, minorVersion);
+	}
 }
