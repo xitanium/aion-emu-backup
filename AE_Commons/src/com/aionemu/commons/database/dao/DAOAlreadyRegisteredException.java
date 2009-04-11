5632fcc24a4103cc87bd01dc02925284860c36a5
SoulKeeper <SoulKeeper@2af79824-a935-44bf-bf41-fd8ed1897d0c>
2009-04-11 11:30:06 +0000

    DAO classes
    git-svn-id: http://my-svn.assembla.com/svn/aion-emu/trunk@77 2af79824-a935-44bf-bf41-fd8ed1897d0c

---
+++ AE_Commons/src/com/aionemu/commons/database/dao/DAOAlreadyRegisteredException.java
@@ -0,0 +1,53 @@
+/*
+ * This file is part of aion-emu.
+ *
+ * aion-emu is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aion-emu is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with aion-emu.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.aionemu.commons.database.dao;
+
+/**
+ * This exception is thrown if DAO is already registered in {@link com.aionemu.commons.database.dao.DAOManager}
+ *
+ * @author SoulKeeper
+ */
+public class DAOAlreadyRegisteredException extends DAOException {
+
+    /**
+     * {@inheritDoc}
+     */
+    public DAOAlreadyRegisteredException() {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public DAOAlreadyRegisteredException(String message) {
+        super(message);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public DAOAlreadyRegisteredException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public DAOAlreadyRegisteredException(Throwable cause) {
+        super(cause);
+    }
+}
