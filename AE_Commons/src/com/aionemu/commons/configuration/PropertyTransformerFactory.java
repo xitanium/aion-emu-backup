7d844f00b9976bc41024c23a756de9b52e2c3d26
SoulKeeper <SoulKeeper@2af79824-a935-44bf-bf41-fd8ed1897d0c>
2009-05-23 14:39:14 +0000

    java.util.regex.Pattern is now detected automatically by PropertyTransformer
    git-svn-id: http://my-svn.assembla.com/svn/aion-emu/trunk@134 2af79824-a935-44bf-bf41-fd8ed1897d0c

--- AE_Commons/src/com/aionemu/commons/configuration/PropertyTransformerFactory.java
+++ AE_Commons/src/com/aionemu/commons/configuration/PropertyTransformerFactory.java
@@ -19,6 +19,7 @@ package com.aionemu.commons.configuration;
 
 import java.io.File;
 import java.net.InetSocketAddress;
+import java.util.regex.Pattern;
 
 import com.aionemu.commons.configuration.transformers.BooleanTransformer;
 import com.aionemu.commons.configuration.transformers.ByteTransformer;
@@ -32,6 +33,7 @@ import com.aionemu.commons.configuration.transformers.IntegerTransformer;
 import com.aionemu.commons.configuration.transformers.LongTransformer;
 import com.aionemu.commons.configuration.transformers.ShortTransformer;
 import com.aionemu.commons.configuration.transformers.StringTransformer;
+import com.aionemu.commons.configuration.transformers.PatternTransformer;
 import com.aionemu.commons.utils.ClassUtils;
 
 /**
@@ -131,6 +133,10 @@ public class PropertyTransformerFactory
 			else if (ClassUtils.isSubclass(clazzToTransform, InetSocketAddress.class))
 			{
 				return InetSocketAddressTransformer.SHARED_INSTANCE;
+			}
+			else if (clazzToTransform == Pattern.class)
+			{
+				return PatternTransformer.SHARED_INSTANCE;
 			}
 			else
 			{
