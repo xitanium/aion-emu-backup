0632d0bbf8fbc26190342538f2e6c81001671547
Nemesiss <Nemesiss@2af79824-a935-44bf-bf41-fd8ed1897d0c>
2009-06-17 10:29:28 +0000

    AE_LoginServer: ae_commons update
    git-svn-id: http://my-svn.assembla.com/svn/aion-emu/trunk@148 2af79824-a935-44bf-bf41-fd8ed1897d0c

--- AE_LoginServer/src/com/aionemu/loginserver/network/gameserver/GsServerPacket.java
+++ AE_LoginServer/src/com/aionemu/loginserver/network/gameserver/GsServerPacket.java
@@ -18,100 +18,15 @@ package com.aionemu.loginserver.network.gameserver;
 
 import java.nio.ByteBuffer;
 
+import com.aionemu.commons.network.packet.BaseServerPacket;
+
 /**
  * Base class for every LS -> GameServer Server Packet.
  * 
  * @author -Nemesiss-
  */
-public abstract class GsServerPacket
+public abstract class GsServerPacket extends BaseServerPacket
 {
-	/**
-	 * Write int to buffer.
-	 * 
-	 * @param buf
-	 * @param value
-	 */
-	protected final void writeD(ByteBuffer buf, int value)
-	{
-		buf.putInt(value);
-	}
-
-	/**
-	 * Write short to buffer.
-	 * 
-	 * @param buf
-	 * @param value
-	 */
-	protected final void writeH(ByteBuffer buf, int value)
-	{
-		buf.putShort((short) value);
-	}
-
-	/**
-	 * Write byte to buffer.
-	 * 
-	 * @param buf
-	 * @param value
-	 */
-	protected final void writeC(ByteBuffer buf, int value)
-	{
-		buf.put((byte) value);
-	}
-
-	/**
-	 * Write double to buffer.
-	 * 
-	 * @param buf
-	 * @param value
-	 */
-	protected final void writeF(ByteBuffer buf, double value)
-	{
-		buf.putDouble(value);
-	}
-
-	/**
-	 * Write long to buffer.
-	 * 
-	 * @param buf
-	 * @param value
-	 */
-	protected final void writeQ(ByteBuffer buf, long value)
-	{
-		buf.putLong(value);
-	}
-
-	/**
-	 * Write String to buffer
-	 * 
-	 * @param buf
-	 * @param text
-	 */
-	protected final void writeS(ByteBuffer buf, String text)
-	{
-		if (text == null)
-		{
-			buf.putChar('\000');
-		}
-		else
-		{
-			final int len = text.length();
-			for (int i = 0; i < len; i++)
-				buf.putChar(text.charAt(i));
-			buf.putChar('\000');
-		}
-	}
-
-	/**
-	 * Write byte array to buffer.
-	 * 
-	 * @param buf
-	 * @param data
-	 */
-	protected final void writeB(ByteBuffer buf, byte[] data)
-	{
-		buf.put(data);
-	}
-
 	/**
 	 * Write this packet data for given connection, to given buffer.
 	 * 
@@ -134,18 +49,4 @@ public abstract class GsServerPacket
 	 * @param buf
 	 */
 	protected abstract void writeImpl(GsConnection con, ByteBuffer buf);
-
-	/**
-	 * @return String - packet name.
-	 */
-	@Override
-	public String toString()
-	{
-		return getType();
-	}
-
-	/**
-	 * @return String - packet name.
-	 */
-	public abstract String getType();
 }
