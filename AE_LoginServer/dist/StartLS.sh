18597af4972ef03bfa314fae8992c221328035af
SoulKeeper <SoulKeeper@2af79824-a935-44bf-bf41-fd8ed1897d0c>
2009-07-05 16:13:57 +0000

    AE_LoginServer: Fix for unix startup script, we don't have stable versions yet :)
    git-svn-id: http://my-svn.assembla.com/svn/aion-emu/trunk@204 2af79824-a935-44bf-bf41-fd8ed1897d0c

--- AE_LoginServer/dist/StartLS.sh
+++ AE_LoginServer/dist/StartLS.sh
@@ -1,3 +1,3 @@
 #!/bin/
-java -Xms8m -Xmx32m -da -dsa -Xbootclasspath/p:./libs/jsr166.jar -cp ./libs/*:ae_login.jar com.aionemu.loginserver.LoginServer
+java -Xms8m -Xmx32m -ea -Xbootclasspath/p:./libs/jsr166.jar -cp ./libs/*:ae_login.jar com.aionemu.loginserver.LoginServer
 
