a37e2944c5aea6c6fc40c1240d543b27b40b6a8f
SoulKeeper <SoulKeeper@2af79824-a935-44bf-bf41-fd8ed1897d0c>
2009-08-23 09:13:38 +0000

    AE_Commons: Just a note
    git-svn-id: http://my-svn.assembla.com/svn/aion-emu/trunk@530 2af79824-a935-44bf-bf41-fd8ed1897d0c

--- AE_Commons/src/com/aionemu/commons/objects/filter/ObjectFilter.java
+++ AE_Commons/src/com/aionemu/commons/objects/filter/ObjectFilter.java
@@ -8,6 +8,8 @@ package com.aionemu.commons.objects.filter;
  * <p/>
  * Created on: 31.03.2009 10:07:12
  *
+ * // TODO: It can be replaced with CommonsCollections.jar Predicate.class. We don't need to reimplement the wheel.
+ * 
  * @author Aquanox
  */
 public interface ObjectFilter<T>
