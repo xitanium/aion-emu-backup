2d5f290ef0f96e14f5037a9ba0f602c409570d61
Xavier Parizet <xavier@games.xen.linuxant.fr>
2009-09-24 20:25:14 +0200

    Apply of patch f1775ad9d.txt : Skills

---
+++ AE_GameServer/src/com/aionemu/gameserver/network/aion/serverpackets/SM_ATTACK.java
@@ -0,0 +1,51 @@
+/**
+ * This file is part of aion-emu <aion-emu.com>.
+ *
+ *  aion-emu is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  aion-emu is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with aion-emu.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.aionemu.gameserver.network.aion.serverpackets;
+
+import java.nio.ByteBuffer;
+
+import com.aionemu.gameserver.network.aion.AionConnection;
+import com.aionemu.gameserver.network.aion.AionServerPacket;
+import com.aionemu.gameserver.network.aion.Version;
+import java.util.Random;
+/**
+ * @author -Avol-
+ * 
+ */
+public class SM_ATTACK extends AionServerPacket
+{
+
+	private int	receiver;
+	
+	
+	public SM_ATTACK(int receiver)
+	{
+		this.receiver = receiver;
+	}
+
+
+	@Override
+	protected void writeImpl(AionConnection con, ByteBuffer buf)
+	{
+		Random generator = new Random();
+		int randomIndex = generator.nextInt(100)+ 1;
+		randomIndex = randomIndex * -1;
+		writeD(buf, receiver);
+		writeD(buf, randomIndex);
+		writeD(buf, 7);
+	}
+}
