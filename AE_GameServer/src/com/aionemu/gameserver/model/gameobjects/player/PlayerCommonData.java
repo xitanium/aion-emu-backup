84eccd5703acf1c3da58daee90d5b1d990069d07
Xavier Parizet <xav@gentooist.com>
2009-09-28 14:55:18 +0200

    Ajout traces pour level up

--- AE_GameServer/src/com/aionemu/gameserver/model/gameobjects/player/PlayerCommonData.java
+++ AE_GameServer/src/com/aionemu/gameserver/model/gameobjects/player/PlayerCommonData.java
@@ -108,18 +108,17 @@ public class PlayerCommonData
 		else
 		{
 			this.exp = exp;
-			
-			if(this.getPlayer()!=null){
-				PacketSendUtility.sendPacket(
-					this.getPlayer(),
-					new SM_STATUPDATE_EXP(
-						this.getExpShown(),
-						0,
-						this.getExpNeed()
-					)
-				);
-				PacketSendUtility.broadcastPacket(this.getPlayer(), new SM_PLAYER_INFO(this.getPlayer(), false), false);
-			}
+		}
+		if(this.getPlayer()!=null){
+			PacketSendUtility.sendPacket(
+				this.getPlayer(),
+				new SM_STATUPDATE_EXP(
+					this.getExpShown(),
+					0,
+					this.getExpNeed()
+				)
+			);
+			PacketSendUtility.broadcastPacket(this.getPlayer(), new SM_PLAYER_INFO(this.getPlayer(), false), false);
 		}
 	}
 
@@ -203,6 +202,7 @@ public class PlayerCommonData
 	
 	public void setLevel(int level)
 	{
+		log.info("NEW LEVEL: "+level);
 		if (level <= DataManager.PLAYER_EXPERIENCE_TABLE.getMaxLevel())
 		{
 			this.level = level;
