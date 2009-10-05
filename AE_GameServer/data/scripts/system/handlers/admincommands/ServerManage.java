65c0845221e1f733fb81f7f0ad440e40548d9e4e
root <root@xitanium-aiondev.(none)>
2009-10-05 13:35:40 +0200

    New admin command : //servermanage [stop|restart|info]

--- AE_GameServer/data/scripts/system/handlers/admincommands/ServerManage.java
+++ AE_GameServer/data/scripts/system/handlers/admincommands/ServerManage.java
@@ -13,8 +13,25 @@ public class ServerManage extends AdminCommand {
 	}
 	
 	public void executeCommand(Player admin, String[] params) {
-		World w = admin.getActiveRegion().getWorld();
-		ShutdownHook sdh = new ShutdownHook(w);
-		PacketSendUtility.sendMessage(admin, "Reboot started");
+		if(params == null || params.length != 1) {
+			PacketSendUtility.sendMessage(admin, "usage: //servermanage [stop|restart|info]");
+		}
+		else 
+		{
+			if(params[0] == "stop") {
+				World w = admin.getActiveRegion().getWorld();
+				ShutdownHook sdh = new ShutdownHook(w);
+				PacketSendUtility.sendMessage(admin, "Reboot started");
+			}
+			else if(params[0] == "restart") {	
+				PacketSendUtility.sendMessage(admin, "method not yet implemented");
+			}
+			else if(params[0] == "info") {
+				PacketSendUtility.sendMessage(admin, "method not yet implemented");
+			}
+			else {
+				PacketSendUtility.sendMessage(admin, "invalid action specified");
+			}
+		}
 	}
 }
