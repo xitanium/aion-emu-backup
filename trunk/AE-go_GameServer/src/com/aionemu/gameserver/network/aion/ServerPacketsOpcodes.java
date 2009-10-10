5465c20439c93e907b3728fb7088382c41421585
xitanium <xitanium@xitanium-osol.(none)>
2009-10-10 13:42:35 +0200

    Added opcode 0xAE to SM_PLAYER_TITLES

--- trunk/AE-go_GameServer/src/com/aionemu/gameserver/network/aion/ServerPacketsOpcodes.java
+++ trunk/AE-go_GameServer/src/com/aionemu/gameserver/network/aion/ServerPacketsOpcodes.java
@@ -131,6 +131,7 @@ public class ServerPacketsOpcodes
 		addPacketOpcode(SM_DUEL_CONFIRMED.class, 0xD0, idSet);
 		addPacketOpcode(SM_DUEL_ACCEPTED.class, 0x68, idSet);
 		
+		addPacketOpcode(SM_PLAYER_TITLES.class, 0xAE, idSet); // 1.5.x player titles
 		
 
 
