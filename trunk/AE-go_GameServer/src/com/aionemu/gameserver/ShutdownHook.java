a7d95bdd60c991ecc0868a8f6fec3549d3a11ce0
xitanium <xitanium@xitanium-osol.(none)>
2009-10-10 15:26:32 +0200

    [auto message] xitanium code update commit

--- trunk/AE-go_GameServer/src/com/aionemu/gameserver/ShutdownHook.java
+++ trunk/AE-go_GameServer/src/com/aionemu/gameserver/ShutdownHook.java
@@ -54,7 +54,7 @@ public class ShutdownHook implements Runnable
 	{
 		log.info("Starting AE GS shutdown sequence");
 		
-		for(int i = 0; i <= Config.SERVER_SHUTDOWN_WAIT_TIME; i--)
+		for(int i = 0; i <= Config.SERVER_SHUTDOWN_WAIT_TIME; i++)
 		{
 			Iterator<Player> onlinePlayers = world.getPlayersIterator();
 			if (!onlinePlayers.hasNext()) {
