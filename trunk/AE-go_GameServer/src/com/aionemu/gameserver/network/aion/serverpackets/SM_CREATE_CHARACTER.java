ea765ab4f21d7a3977579390ab5b2b289f66421d
Xavier Parizet <xav@gentooist.com>
2009-10-13 03:13:31 +0200

    SM_CREATE_CHARACTER corrigé
    	* Ajout de l'équipement dans la db avant de les équiper

--- trunk/AE-go_GameServer/src/com/aionemu/gameserver/network/aion/serverpackets/SM_CREATE_CHARACTER.java
+++ trunk/AE-go_GameServer/src/com/aionemu/gameserver/network/aion/serverpackets/SM_CREATE_CHARACTER.java
@@ -24,6 +24,7 @@ import com.aionemu.gameserver.model.account.PlayerAccountData;
 import com.aionemu.gameserver.network.aion.AionConnection;
 import com.aionemu.gameserver.network.aion.PlayerInfo;
 import com.aionemu.gameserver.model.gameobjects.player.Inventory;
+
 /**
  * This packet is response for CM_CREATE_CHARACTER
  * 
@@ -33,31 +34,28 @@ import com.aionemu.gameserver.model.gameobjects.player.Inventory;
 public class SM_CREATE_CHARACTER extends PlayerInfo
 {
 	/** If response is ok */
-	public static final int	RESPONSE_OK				= 0x00;
-	
-	
-	public static final int	FAILED_TO_CREATE_THE_CHARACTER = 1;
-	/** Failed to create the character due to world db error */
-	public static final int RESPONSE_DB_ERROR = 2;
-	/** The number of characters exceeds the maximum allowed for the server */
-	public static final int RESPONSE_SERVER_LIMIT_EXCEEDED = 4;
-	/** Invalid character name */
-	public static final int	RESPONSE_INVALID_NAME	= 5;
-	/** The name includes forbidden words */
-	public static final int RESPONSE_FORBIDDEN_CHAR_NAME= 9;
-	/** A character with that name already exists */
-	public static final int	RESPONSE_NAME_ALREADY_USED			= 10;
-	/** The name is already reserved */
-	public static final int RESPONSE_NAME_RESERVED = 11;
-	/** You cannot create characters of other races in the same server */
-	public static final int RESPONSE_OTHER_RACE = 12;
+	public static final int			RESPONSE_OK						= 0x00;
 
-	
+	public static final int			FAILED_TO_CREATE_THE_CHARACTER	= 1;
+	/** Failed to create the character due to world db error */
+	public static final int			RESPONSE_DB_ERROR				= 2;
+	/** The number of characters exceeds the maximum allowed for the server */
+	public static final int			RESPONSE_SERVER_LIMIT_EXCEEDED	= 4;
+	/** Invalid character name */
+	public static final int			RESPONSE_INVALID_NAME			= 5;
+	/** The name includes forbidden words */
+	public static final int			RESPONSE_FORBIDDEN_CHAR_NAME	= 9;
+	/** A character with that name already exists */
+	public static final int			RESPONSE_NAME_ALREADY_USED		= 10;
+	/** The name is already reserved */
+	public static final int			RESPONSE_NAME_RESERVED			= 11;
+	/** You cannot create characters of other races in the same server */
+	public static final int			RESPONSE_OTHER_RACE				= 12;
 
 	/**
 	 * response code
 	 */
-	private final int		responseCode;
+	private final int				responseCode;
 
 	/**
 	 * Newly created player.
@@ -91,35 +89,51 @@ public class SM_CREATE_CHARACTER extends PlayerInfo
 		{
 			Inventory inventory = new Inventory();
 			int activePlayer = player.getPlayerCommonData().getPlayerObjId();
-			if (player.getPlayerCommonData().getPlayerClass().getClassId() == 0) {// warrior
-					inventory.putIsEquipedToDb(activePlayer, 100000094, ItemSlot.MAIN_HAND);//sword for training
-					inventory.putIsEquipedToDb(activePlayer, 110500003, ItemSlot.ARMOR);//Chain armor for training
-					inventory.putIsEquipedToDb(activePlayer, 113500001, ItemSlot.PANTS);//Chain Greaves for training
-					inventory.putItemToDb(activePlayer, 160000001, 12); // mercenary Fruit Juice
-					inventory.putItemToDb(activePlayer, 169300001, 20); // bandage
+			if(player.getPlayerCommonData().getPlayerClass().getClassId() == 0)
+			{// warrior
+				inventory.putItemToDb(activePlayer, 100000094, 1);
+				inventory.putIsEquipedToDb(activePlayer, 100000094, ItemSlot.MAIN_HAND);// sword for training
+				inventory.putItemToDb(activePlayer, 110500003, 1);
+				inventory.putIsEquipedToDb(activePlayer, 110500003, ItemSlot.ARMOR);// Chain armor for training
+				inventory.putItemToDb(activePlayer, 113500001, 1);
+				inventory.putIsEquipedToDb(activePlayer, 113500001, ItemSlot.PANTS);// Chain Greaves for training
+				inventory.putItemToDb(activePlayer, 160000001, 12); // mercenary Fruit Juice
+				inventory.putItemToDb(activePlayer, 169300001, 20); // bandage
 			}
-			if (player.getPlayerCommonData().getPlayerClass().getClassId() == 3) { //SCOUT	
-					inventory.putIsEquipedToDb(activePlayer, 100200112, ItemSlot.MAIN_HAND);//Dagger for training
-					inventory.putIsEquipedToDb(activePlayer, 110300015, ItemSlot.ARMOR);//Leather Jerkin for training
-					inventory.putIsEquipedToDb(activePlayer, 113300005, ItemSlot.PANTS);//Leather leggins for training
-					inventory.putItemToDb(activePlayer, 160000001, 12); // mercenary Fruit Juice
-					inventory.putItemToDb(activePlayer, 169300001, 20); // bandage
+			if(player.getPlayerCommonData().getPlayerClass().getClassId() == 3)
+			{ // SCOUT
+				inventory.putItemToDb(activePlayer, 100200112, 1);
+				inventory.putIsEquipedToDb(activePlayer, 100200112, ItemSlot.MAIN_HAND);// Dagger for training
+				inventory.putItemToDb(activePlayer, 110300015, 1);
+				inventory.putIsEquipedToDb(activePlayer, 110300015, ItemSlot.ARMOR);// Leather Jerkin for training
+				inventory.putItemToDb(activePlayer, 113300005, 1);
+				inventory.putIsEquipedToDb(activePlayer, 113300005, ItemSlot.PANTS);// Leather leggins for training
+				inventory.putItemToDb(activePlayer, 160000001, 12); // mercenary Fruit Juice
+				inventory.putItemToDb(activePlayer, 169300001, 20); // bandage
 			}
-			if (player.getPlayerCommonData().getPlayerClass().getClassId() == 6) {//MAGE
-					inventory.putIsEquipedToDb(activePlayer, 100600034, ItemSlot.MAIN_HAND);//SpellBook for training
-					inventory.putIsEquipedToDb(activePlayer, 110100009, ItemSlot.ARMOR);//Cloth Tunic for Training
-					inventory.putIsEquipedToDb(activePlayer, 113100005, ItemSlot.PANTS);//Cloth Leggins for training
-					inventory.putItemToDb(activePlayer, 160000001, 12); // mercenary Fruit Juice
-					inventory.putItemToDb(activePlayer, 169300001, 20); // bandage
+			if(player.getPlayerCommonData().getPlayerClass().getClassId() == 6)
+			{// MAGE
+				inventory.putItemToDb(activePlayer, 100600034, 1);
+				inventory.putIsEquipedToDb(activePlayer, 100600034, ItemSlot.MAIN_HAND);// SpellBook for training
+				inventory.putItemToDb(activePlayer, 110100009, 1);
+				inventory.putIsEquipedToDb(activePlayer, 110100009, ItemSlot.ARMOR);// Cloth Tunic for Training
+				inventory.putItemToDb(activePlayer, 113100005, 1);
+				inventory.putIsEquipedToDb(activePlayer, 113100005, ItemSlot.PANTS);// Cloth Leggins for training
+				inventory.putItemToDb(activePlayer, 160000001, 12); // mercenary Fruit Juice
+				inventory.putItemToDb(activePlayer, 169300001, 20); // bandage
 			}
-			if (player.getPlayerCommonData().getPlayerClass().getClassId() == 9) {//PRIEST
-					inventory.putIsEquipedToDb(activePlayer, 100100011, ItemSlot.MAIN_HAND);//Mace for training
-					inventory.putIsEquipedToDb(activePlayer, 110300292, ItemSlot.ARMOR);//Leather armor for training
-					inventory.putIsEquipedToDb(activePlayer, 113300278, ItemSlot.PANTS);//Leather leg armor for training
-					inventory.putItemToDb(activePlayer, 160000001, 12); // mercenary Fruit Juice
-					inventory.putItemToDb(activePlayer, 169300001, 20); // bandage
+			if(player.getPlayerCommonData().getPlayerClass().getClassId() == 9)
+			{// PRIEST
+				inventory.putItemToDb(activePlayer, 100100011, 1);
+				inventory.putIsEquipedToDb(activePlayer, 100100011, ItemSlot.MAIN_HAND);// Mace for training
+				inventory.putItemToDb(activePlayer, 110300292, 1);
+				inventory.putIsEquipedToDb(activePlayer, 110300292, ItemSlot.ARMOR);// Leather armor for training
+				inventory.putItemToDb(activePlayer, 113300278, 1);
+				inventory.putIsEquipedToDb(activePlayer, 113300278, ItemSlot.PANTS);// Leather leg armor for training
+				inventory.putItemToDb(activePlayer, 160000001, 12); // mercenary Fruit Juice
+				inventory.putItemToDb(activePlayer, 169300001, 20); // bandage
 			}
-			
+
 			writePlayerInfo(buf, player); // if everything is fine, all the character's data should be sent
 			writeD(buf, player.getDeletionTimeInSeconds());
 			writeD(buf, 0x00);// unk
