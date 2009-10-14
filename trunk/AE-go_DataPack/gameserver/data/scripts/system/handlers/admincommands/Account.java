9f5476dd5a1763831a29d0cc2c2f527aabdb9966
xitanium <xitanium@xitanium-osol.(none)>
2009-10-14 05:17:13 +0200

    Added //account < returns gmlevel of current account (requires gmlevel 0)

---
+++ trunk/AE-go_DataPack/gameserver/data/scripts/system/handlers/admincommands/Account.java
@@ -0,0 +1,46 @@
+/**
+ * This file is part of aion-emu <aion-emu.com>.
+ *
+ *  aion-emu is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  aion-emu is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with aion-emu.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Xitanium
+ *
+ */
+
+package admincommands;
+
+import com.aionemu.gameserver.model.AdminLevel;
+import com.aionemu.gameserver.model.gameobjects.player.Player;
+import com.aionemu.gameserver.utils.PacketSendUtility;
+import com.aionemu.gameserver.utils.chathandlers.AdminCommand;
+
+public class Account extends AdminCommand
+{
+	/**
+	 * @param commandName
+	 */
+	public Account()
+	{
+		super("account", AdminLevel.PLAYER);
+	}
+
+	/* (non-Javadoc)
+	 * @see com.aionemu.gameserver.utils.chathandlers.admincommands.AdminCommand#executeCommand(com.aionemu.gameserver.gameobjects.Player, java.lang.String[])
+	 */
+	@Override
+	public void executeCommand(Player admin, String[] params)
+	{
+		PacketSendUtility.sendMessage(admin, "Your account level is: " + admin.getCommonData().getAdminLevel());
+	}
+}
